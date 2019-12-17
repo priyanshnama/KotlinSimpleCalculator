8d16b1c037f31d0089e34c32fbde8db2d77479c6
Priyansh Nama <priyanshnama@gmail.com>
2019-12-17 20:54:24 +0530

    version 1.0 completed

--- app/src/main/java/com/priyanshnama/kotlinsimplecalculator/MainActivity.kt
+++ app/src/main/java/com/priyanshnama/kotlinsimplecalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Double.parseDouble
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,100 +14,103 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun add(view: View){
+
         val str1 = number1.text.toString()
-        val num1:Int
-        if(str1.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num1 = str1.toInt()
-        }
-        else{
+        val num1:Double
+        try {
+            num1 = str1.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
 
         val str2 = number2.text.toString()
-        val num2:Int
-        if(str2.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num2 = str1.toInt()
-        }
-        else{
+        val num2:Double
+        try {
+            num2 = str2.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
 
         val result = num1 + num2
+        println("$num1 + $num2 = $result")
         textView.text = "" + result
     }
 
     fun subtract(view: View){
+
         val str1 = number1.text.toString()
-        val num1:Int
-        if(str1.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num1 = str1.toInt()
-        }
-        else{
+        val num1:Double
+        try {
+            num1 = str1.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
 
         val str2 = number2.text.toString()
-        val num2:Int
-        if(str2.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num2 = str1.toInt()
-        }
-        else{
+        val num2:Double
+        try {
+            num2 = str2.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
+
         val result = num1 - num2
+        println("$num1 - $num2 = $result")
         textView.text = "" + result
     }
 
     fun multiply(view: View){
+
         val str1 = number1.text.toString()
-        val num1:Int
-        if(str1.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num1 = str1.toInt()
-        }
-        else{
+        val num1:Double
+        try {
+            num1 = str1.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
 
         val str2 = number2.text.toString()
-        val num2:Int
-        if(str2.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num2 = str1.toInt()
-        }
-        else{
+        val num2:Double
+        try {
+            num2 = str2.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
+
         val result = num1 * num2
+        println("$num1 * $num2 = $result")
         textView.text = "" + result
     }
 
     fun divide(view: View){
+
         val str1 = number1.text.toString()
-        val num1:Int
-        if(str1.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num1 = str1.toInt()
-        }
-        else{
+        val num1:Double
+        try {
+            num1 = str1.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
 
         val str2 = number2.text.toString()
-        val num2:Int
-        if(str2.matches("-?\\d+(\\.\\d+)?".toRegex())){
-            num2 = str1.toInt()
-        }
-        else{
+        val num2:Double
+        try {
+            num2 = str2.toDouble()
+        } catch (e: NumberFormatException) {
             textView.text = "Enter something valid"
             return
         }
+
         val result: Double
-        result = (num1 / num2) * 1.00
+        result = num1 / num2
+        println("$num1 / $num2 = $result")
         textView.text = "" + result
     }
 }
