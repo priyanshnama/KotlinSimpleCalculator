c15beae5d18bff8cde1a276913c427c94bbdce33
Priyansh Nama <priyanshnama@gmail.com>
2019-12-17 22:36:51 +0530

    version 1.0 completed

--- app/src/main/java/com/priyanshnama/kotlinsimplecalculator/MainActivity.kt
+++ app/src/main/java/com/priyanshnama/kotlinsimplecalculator/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Double.parseDouble
 
 class MainActivity : AppCompatActivity() {
 
