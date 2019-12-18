a839f9d8e60d0457f1001d93d91fde6001b19c0d
Priyansh Nama <priyanshnama@gmail.com>
2019-12-18 14:08:07 +0530

    version 1.0 completed

--- app/src/main/java/com/priyanshnama/kotlinsimplecalculator/MainActivity.kt
+++ app/src/main/java/com/priyanshnama/kotlinsimplecalculator/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,8 +53,24 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val result = num1 - num2
-        println("$num1 - $num2 = $result")
+        val ind1 = str1.indexOf('.')
+        val ind2 = str2.indexOf('.')
+
+        val len1 = str1.length
+        val len2 = str2.length
+
+        val scale1 = len1 - (ind1 + 1)
+        val scale2 = len2 - (ind2 + 1)
+
+        val scale : Int
+        if(scale1 > scale2)
+            scale = scale1
+        else
+            scale = scale2
+
+        val res = num1 - num2
+        val result = BigDecimal(res).setScale(scale, RoundingMode.HALF_EVEN)
+        println("$num1 - $num2 = $result process: len1=$len1 len2=$len2 ind1=$ind1 ind2=$ind2 scale1:$scale1 scale2:$scale2 scale:$scale")
         textView.text = "" + result
     }
 
@@ -73,8 +91,19 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val result = num1 * num2
-        println("$num1 * $num2 = $result")
+        val ind1 = str1.indexOf('.')
+        val ind2 = str2.indexOf('.')
+
+        val len1 = str1.length
+        val len2 = str2.length
+
+        val scale1 = len1 - (ind1 + 1)
+        val scale2 = len2 - (ind2 + 1)
+
+        val scale = scale1 + scale2
+        val res = num1 * num2
+        val result = BigDecimal(res).setScale(scale, RoundingMode.HALF_EVEN)
+        println("$num1 - $num2 = $result process: len1=$len1 len2=$len2 ind1=$ind1 ind2=$ind2 scale1:$scale1 scale2:$scale2 scale:$scale")
         textView.text = "" + result
     }
 
